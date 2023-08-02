935ad94ba7468896562eedc92729feb55af7c5a7
ditusnova <ditusnova1@gmail.com>
2023-08-02 18:00:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
   
+const hireMeButton = document.querySelector('.cta-button');
+hireMeButton.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Change "your_email@example.com" to your actual email address
+  const email = 'your_email@example.com';
+  const subject = 'Hiring Inquiry';
+
+  const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}`;
+
+  window.location.href = mailtoLink;
+});

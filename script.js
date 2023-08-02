b07e201a89e3b55efec7c19827bea3ece20c8843
ditusnova <ditusnova1@gmail.com>
2023-08-02 18:15:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-// Smooth scroll to anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-  
-      const target = document.querySelector(this.getAttribute('href'));
-      const offset = 50;
-      const elementPosition = target.getBoundingClientRect().top;
-      const offsetPosition = elementPosition - offset;
-  
-      window.scrollBy({
-        top: offsetPosition,
-        behavior: 'smooth'
-      });
-    });
-  });
+
   
 const hireMeButton = document.querySelector('.cta-button');
 hireMeButton.addEventListener('click', function (e) {
